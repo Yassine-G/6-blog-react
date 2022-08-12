0fc176d5870b36d8891ef900eb5d09f33f1278bc
Your Name <yassineghoudane@gmail.com>
2022-08-12 15:48:23 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import Navbar from "./Components/Navbar/Navbar";
+import Home from "./Containers/Home/Home";
+import { Routes, Route } from "react-router-dom";
+import AddArticle from "./Containers/AddArticle/AddArticle";
+import Contact from "./Containers/Contact/Contact";
+import Article from "./Containers/Article/Article";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />}/>
+        <Route path="/ecrire" element={<AddArticle />}/>
+        <Route path="/contact" element={<Contact />}/>
+        <Route path="/articles/:slug" element={<Article />}/>
+      </Routes>
+    </>
+  );
+}
+
+export default App;
